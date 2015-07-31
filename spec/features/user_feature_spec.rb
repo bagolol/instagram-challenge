6890118ba82453ea3320728aee2e5bbe1b219f2b
Rocco <roccorighi@hotmail.com>
2015-07-31 14:14:30 +0100

    adds missing tag to layout head

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -35,4 +35,21 @@ feature "User can sign in and out" do
       expect(page).not_to have_link('Sign up')
     end
   end
+  context "user signed in signs out" do
+    before do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password', with: 'testtest')
+      fill_in('Password confirmation', with: 'testtest')
+      click_button('Sign up')
+    end
+
+    it "can sign out" do
+      visit('/')
+      expect(page).to have_link('Sign out')
+      click_link('Sign out')
+      expect(page).not_to have_link('Sign out')
+    end
+  end
 end
